1f45ff9f57106e313e8feba2b09fb59c3a8792ee
mariyaeggs <mariyawilson@live.com>
2017-04-02 12:57:16 -0400

    first commit

---
+++ AFCC/File.swift
@@ -0,0 +1,9 @@
+//
+//  File.swift
+//  AFCC
+//
+//  Created by student on 4/1/17.
+//  Copyright © 2017 Mariya Eggensperger. All rights reserved.
+//
+
+import Foundation
---
+++ ImageManager.swift
@@ -0,0 +1,9 @@
+//
+//  ImageManager.swift
+//  
+//
+//  Created by student on 4/1/17.
+//
+//
+
+import Foundation
