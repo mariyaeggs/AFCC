8d51021297c10763eb208688a321f5b36c383fbd
mariyaeggs <mariyawilson@live.com>
2017-04-06 11:54:22 -0400

    Application saves profile data to Firebase, but crashes with nil unwrapped.

--- AFCC/SignUpViewController.swift
+++ AFCC/SignUpViewController.swift
@@ -15,25 +15,48 @@ import FirebaseAuth
 import FirebaseDatabase
 import FirebaseStorage
 
+
 class SignUpViewController: UIViewController, UITextFieldDelegate, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
    
+   var imagePicker: UIImagePickerController!
+   var isImageSelected = false
+
    // MARK: Outlets
    @IBOutlet weak var emailTextField: UITextField!
    @IBOutlet weak var passwordTextField: UITextField!
    @IBOutlet weak var nameTextField: UITextField!
    @IBOutlet weak var profileImageView: UIImageView!
    
-   // Rounded user image upload
+   // Additional setup after loading the view
    override func viewDidLoad() {
       super.viewDidLoad()
+      // Rounded rect images
       profileImageView.layer.cornerRadius =
          profileImageView.frame.size.width/2
       profileImageView.clipsToBounds = true
+      
+      nameTextField.delegate = self;
+      imagePicker = UIImagePickerController()
+      imagePicker.delegate = self
+      imagePicker.allowsEditing = true
       
    }
    
    // MARK: Actions
    @IBAction func signUpAction(_sender: AnyObject) {
+      guard let nameField = nameTextField.text, nameField != "" else {
+         print("Enter name.")
+         return
+      }
+      
+      // Select an image from iPhoto
+      guard let img = profileImageView.image,
+         isImageSelected == true
+         else {
+            print("Select an image.")
+            return
+      }
+      
       // If the user does not input email/password
       if emailTextField.text == "" {
          // Show alert and request for email/password
@@ -61,10 +84,74 @@ class SignUpViewController: UIViewController, UITextFieldDelegate, UIImagePicker
                
                self.present(alertController, animated: true, completion: nil)
             }
+            if let imgData = UIImageJPEGRepresentation(img, 0.2) {
+               // setting a unique identifier
+               let imgUid = NSUUID().uuidString
+               // letting it know itll be a jpeg for safety
+               let metaData = FIRStorageMetadata()
+               metaData.contentType = "image/jpeg"
+               // Referencing Firebase storage child with the unique identifier, and updating with the image from the picker
+               DataManager.shared.REF_PROFILE_IMAGES.child(imgUid).put(imgData, metadata: metaData, completion: { (metadata, error) in
+                  if error != nil {
+                     print("Unable to upload image Firebase storage")
+                  } else {
+                     print("Successfully uploaded image to Firebase storage")
+                     let downloadUrl = metadata?.downloadURL()?.absoluteString
+                     if let url = downloadUrl {
+                        //once the image is uploaded to firebase stoarge, its then posted to the database
+                        self.postToFirebase(imgUrl: url)
+                     }
+                  }
+               })
+               
+
          }
       }
+         // compressing the post image for Firebase storage
+}//opening app
       
+
    }
+   func postToFirebase(imgUrl: String) {
+      // setting up how I will update that specific users child value
+      let userInfo: Dictionary<String, Any> = [
+         "name" : nameTextField.text!,
+         "profile-pic" : imgUrl
+         
+      ]
+      
+      // updating the database specific user with new information
+      DataManager.shared.REF_USER_CURRENT.updateChildValues(userInfo)
+      
+      // resset all fields
+      nameTextField.text = ""
+      isImageSelected = false
+      
+      
+      profileImageView.image = UIImage(named: "default-pic")
+   }
+
+   
+   @IBAction func profilePicTapped(_ sender: AnyObject) {
+      present(imagePicker, animated: true, completion: nil)
+   }
+   
+   // Mark: UIImagePickerControllerDelegate
+   func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String : Any]) {
+      if let img = info[UIImagePickerControllerEditedImage] as? UIImage {
+         profileImageView.image = img
+         isImageSelected = true
+      } else {
+         print ("Invalid image selected.")
+      }
+      imagePicker.dismiss(animated: true, completion: nil)
+   }
+   // Mark: UITextFieldDelegate
+   func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+      self.view.endEditing(true)
+      return true
+   }
+   
 }
 
 
